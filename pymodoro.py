437a2f5e38259a9f292d7c6a8ca0375aa19c6ea0
Dominick Vale <dominickveil@gmail.com>
2020-08-27 21:07:36 +0200

    FIx output on windows

--- pymodoro.py
+++ pymodoro.py
@@ -1,32 +1,37 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
+from blessed import Terminal
 import argparse
 import time
 import threading
 import sys
-from blessed import Terminal
+import warnings
 
+warnings.filterwarnings('ignore')
 parser = argparse.ArgumentParser()
-parser.add_argument('-wm', type=float, help='Working minutes')
-parser.add_argument('-bm', type=float, help='Break minutes')
+parser.add_argument('-w', type=float, help='Working minutes')
+parser.add_argument('-b', type=float, help='Break minutes')
 parser.add_argument('start', help='Start timer')
-
-timeLeft = 25
+args = parser.parse_args()
 
 inputChar = ''          # Character to be used to eventually stop the program
 t = Terminal()
 
+timeLeft = 60 * args.w
+
 
 def quitPomodoro():
+    print('\n'*10)
     sys.exit()
 
 def printStatus():
+    print('\n\n')
     print(t.center("", fillchar="@"))
     print("@" + t.move_right(t.width) + "@")
     print(t.center(f"    Time remaining: {t.bold + str(timeLeft)}    ", fillchar="~"))
     print("@" + t.move_right(t.width) + "@")
     print(t.center("", fillchar="@"))
-    print(t.move_up(6))
+    print("\033[9A")
 
 
 def checkInput():
@@ -34,22 +39,21 @@ def checkInput():
     Loop that gets the input character in the terminal every second.
     """
     global inputChar
-    with t.hidden_cursor():     # Hide cursor
-        with t.cbreak():
-            while True:
-                inputChar = t.inkey(timeout=1)
+    while True:
+        inputChar = t.inkey()
 
 
 def loop():
     global timeLeft
-    while timeLeft >= 0 and inputChar.lower() != 'q':
-        try:
-            printStatus()
-            timeLeft -= 1
-            time.sleep(1)
-        except (KeyboardInterrupt, Exception):
-            quitPomodoro()
-    quitPomodoro()
+    with t.cbreak(), t.hidden_cursor():     # Hide cursor
+        while timeLeft >= 0 and inputChar.lower() != 'q':
+            try:
+                printStatus()
+                timeLeft -= 1
+                time.sleep(1)
+            except (KeyboardInterrupt, Exception):
+                quitPomodoro()
+        quitPomodoro()
 
 
 def main():
